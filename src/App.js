254d9a9f580da660d7c87779cbb2174c9b03a2ee
Aminul Islam <aminulislamroqib@gmail.com>
2021-04-29 02:55:43 +0600

    react-core

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nayoks = ['a', 'b', 'C', 'sd']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-
+        <p>I am a React Developer</p>
+        <Counter></Counter>
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+        </ul>
+        <Person name='Islam' job='JavaScript'></Person>
+        <Person name='Roqib' job='HTML/CSS'></Person>
+        <Person></Person>
       </header>
     </div>
   );
+}
+
+function Counter() {
+  const [count, setCount] = useState(10);
+  const handleCount = () => setCount(count + 1)
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}> increase </button>
+      <button onMouseOver={() => setCount(count - 1)}> decrease </button>
+    </div>  
+  )
+    
+}
+function Person(props) {
+  return (
+    <div style={{ border: "2px solid red", margin: '10px' }}>
+      <h1>Name:{props.name}</h1>
+      <h2>Job:{props.job} </h2>
+
+    </div>
+
+  )
+
 }
 
 export default App;
